ce0901de9c292fdfcfe97e0f5319c8b83b1c6893
soltanoff <piccadillable@gmail.com>
2023-03-02 00:24:48 +0300

    bug fixes and image generation functionality

--- ai.py
+++ ai.py
@@ -1,5 +1,3 @@
-from typing import List
-
 import openai
 
 
@@ -10,21 +8,36 @@ class AIWrapper:
     top_p: float = 1.0
     frequency_penalty: float = 0.0
     presence_penalty: float = 0.0
-    stop: List[str] = ['Q:', 'A:']
+    image_size: str = '512x512'  # '1024x1024'
 
     def __init__(self, token: str):
         openai.api_key = token
 
     @classmethod
-    async def get_answer(cls, message_history: str) -> str:
+    async def get_answer(cls, message: str) -> str:
+        if not message.endswith('.'):
+            message += '.'
+
         response = await openai.Completion.acreate(
             model=cls.model,
-            prompt=message_history,
+            prompt=message,
             temperature=cls.temperature,
             max_tokens=cls.max_tokens,
             top_p=cls.top_p,
             frequency_penalty=cls.frequency_penalty,
             presence_penalty=cls.presence_penalty,
-            # stop=cls.stop,
         )
-        return ''.join(x['text'] for x in response['choices'])
+        answer = response['choices'][0]['text'].strip()
+        if not answer:
+            answer = 'Мне нечего ответить :('
+
+        return answer
+
+    @classmethod
+    async def get_image(cls, message: str) -> str:
+        response = await openai.Image.acreate(
+            prompt=message,
+            n=1,
+            size=cls.image_size,
+        )
+        return f'Готово<a href="{response["data"][0]["url"]}">.</a>'
--- main.py
+++ main.py
@@ -18,6 +18,19 @@ dp = Dispatcher(bot)
 SLEEP_AFTER_EXCEPTION = timedelta(minutes=1).seconds
 
 
+async def get_ai_answer(message: types.Message, request_message: Optional[str] = None) -> Tuple[str, Optional[str]]:
+    chat_id: int = message.chat.id
+    user_id: str = message.from_user.id
+    username: str = message.from_user.username
+    request_message: str = request_message or message.text.strip()
+
+    await message.answer_chat_action('typing')
+    logging.info('>>> User[%s|%s:@%s]: %r', chat_id, user_id, username, request_message)
+    answer = await ai.get_answer(request_message)
+    logging.info('<<< User[%s|%s:@%s]: %r', chat_id, user_id, username, answer)
+    return answer, ParseMode.MARKDOWN if '```' in answer else None
+
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     chat_id: int = message.chat.id
@@ -30,17 +43,38 @@ async def send_welcome(message: types.Message):
     await message.reply('Штош, ну привет!')
 
 
-async def get_ai_answer(message: types.Message, request_message: Optional[str] = None) -> Tuple[str, Optional[str]]:
+@dp.message_handler(commands=['report'])
+async def send_report(message: types.Message):
+    request_message: str = message.text[7:].strip()
+    if len(request_message) < 10:
+        await message.reply('Краткость - сестра таланта, да?')
+        return
+
     chat_id: int = message.chat.id
     user_id: str = message.from_user.id
     username: str = message.from_user.username
-    request_message: str = request_message or message.text.strip()
+
+    await message.answer_chat_action('typing')
+    logging.warning('[REPORT] User[%s|%s:@%s]: %r', chat_id, user_id, username, request_message)
+    await message.reply('✅ Отчет отправлен, спасибо!', parse_mode=ParseMode.HTML)
+
+
+@dp.message_handler(commands=['img'])
+async def send_image(message: types.Message):
+    request_message: str = message.text[4:].strip()
+    if len(request_message) < 3:
+        await message.reply('Краткость - сестра таланта, да?')
+        return
+
+    chat_id: int = message.chat.id
+    user_id: str = message.from_user.id
+    username: str = message.from_user.username
 
     await message.answer_chat_action('typing')
     logging.info('>>> User[%s|%s:@%s]: %r', chat_id, user_id, username, request_message)
-    answer = await ai.get_answer(request_message)
+    answer = await ai.get_image(request_message)
     logging.info('<<< User[%s|%s:@%s]: %r', chat_id, user_id, username, answer)
-    return answer, ParseMode.MARKDOWN if '```' in answer else None
+    await message.reply(answer, parse_mode=ParseMode.HTML)
 
 
 @dp.message_handler(commands=['cat'])
