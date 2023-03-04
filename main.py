7f2ac092d70637bd27d4f71ac9df865a405b0e0e
soltanoff <piccadillable@gmail.com>
2023-03-05 01:46:24 +0300

    added custom decorator for commands callback

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import logging
 import os
 import time
 from datetime import timedelta
-from functools import partial
-from typing import Optional, Tuple
+from functools import partial, wraps
+from typing import Optional, Tuple, Callable
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ParseMode
@@ -17,6 +17,24 @@ ai = AIWrapper(openai_token=os.getenv('OPEANAI_API_KEY'), chat_access_token=os.g
 bot = Bot(token=os.getenv('TELEGRAM_API_KEY'))
 dp = Dispatcher(bot)
 SLEEP_AFTER_EXCEPTION = timedelta(minutes=1).seconds
+MESSAGE_SIZE_LIMIT = 10
+
+
+def special_command_handler(dispatcher: Dispatcher, command: str) -> Callable:
+    def decorator(callback: Callable) -> Callable:
+        @wraps(callback)
+        async def wrapper(message: types.Message) -> None:
+            request_message: str = message.text[len(command):].strip()
+            if len(request_message) < MESSAGE_SIZE_LIMIT:
+                await message.reply('Краткость - сестра таланта, да?')
+                return
+
+            await callback(message, request_message)
+
+        dispatcher.message_handler(commands=[command])(wrapper)
+        return wrapper
+
+    return decorator
 
 
 async def get_ai_answer(message: types.Message, request_message: Optional[str] = None) -> Tuple[str, Optional[str]]:
@@ -37,20 +55,14 @@ async def send_welcome(message: types.Message):
     chat_id: int = message.chat.id
     user_id: str = message.from_user.id
     username: str = message.from_user.username
-    request_message: str = message.text
 
     await message.answer_chat_action('typing')
-    logging.info('>>> User[%s|%s:@%s]: %r', chat_id, user_id, username, request_message)
+    logging.info('>>> User[%s|%s:@%s]: %r', chat_id, user_id, username, message.text)
     await message.reply('Штош, ну привет!')
 
 
-@dp.message_handler(commands=['report'])
-async def send_report(message: types.Message):
-    request_message: str = message.text[7:].strip()
-    if len(request_message) < 10:
-        await message.reply('Краткость - сестра таланта, да?')
-        return
-
+@special_command_handler(dp, command='report')
+async def send_report(message: types.Message, request_message: str):
     chat_id: int = message.chat.id
     user_id: str = message.from_user.id
     username: str = message.from_user.username
@@ -60,13 +72,8 @@ async def send_report(message: types.Message):
     await message.reply('✅ Отчет отправлен, спасибо!', parse_mode=ParseMode.HTML)
 
 
-@dp.message_handler(commands=['img'])
-async def send_image(message: types.Message):
-    request_message: str = message.text[4:].strip()
-    if len(request_message) < 3:
-        await message.reply('Краткость - сестра таланта, да?')
-        return
-
+@special_command_handler(dp, command='img')
+async def send_image(message: types.Message, request_message: str):
     chat_id: int = message.chat.id
     user_id: str = message.from_user.id
     username: str = message.from_user.username
@@ -78,13 +85,8 @@ async def send_image(message: types.Message):
     await message.reply(answer, parse_mode=ParseMode.HTML)
 
 
-@dp.message_handler(commands=['cat'])
-async def send_ai_answer_from_group(message: types.Message):
-    request_message: str = message.text[4:].strip()
-    if len(request_message) < 3:
-        await message.reply('Краткость - сестра таланта, да?')
-        return
-
+@special_command_handler(dp, command='cat')
+async def send_ai_answer_from_group(message: types.Message, request_message: str):
     answer, parse_mode = await get_ai_answer(message, request_message)
     await message.reply(answer, parse_mode=parse_mode)
 
